ac821c28d342c7c3ec267d237f76892e5f1e6019
Martin Anselmo <mfanselmo@uc.cl>
2018-04-08 21:33:06 -0300

    Ahora se puede hacer admin desde register

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
 	devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation, :admin])
 	devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :password, :password_confirmation])
-	devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :current_password])
+	devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :current_password, :admin])
   end
 
 end
