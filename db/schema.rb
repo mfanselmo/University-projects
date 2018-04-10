534220bed70d054d646406628b6d380f651e81b3
RODRIGO HANUCH <rihanuch@uc.cl>
2018-04-10 11:54:02 -0300

    se crean foros, falta implementar link para posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20180408182011) do
-
+ActiveRecord::Schema.define(version: 20180410144736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +22,12 @@ ActiveRecord::Schema.define(version: 20180408182011) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
+  create_table "forums", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
@@ -54,5 +58,4 @@ ActiveRecord::Schema.define(version: 20180408182011) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
-  add_foreign_key "comments", "posts"
 end
