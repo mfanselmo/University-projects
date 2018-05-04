2a0e425ad688421d862505e8e6aa3bb4ad8e6258
nachocontreras <icontreras1@uc.cl>
2018-05-04 12:46:22 -0300

    block forums

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,17 @@
 class CommentsController < ApplicationController
   # http_basic_authenticate_with :name => "dhh", :password => "secret", :only => :destroy
 
+  before_action :find_post, only: [:edit]
+
+
+  def index
+    @comments = Comments.all
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @comments }
+    end
+  end
+
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment].permit(:commenter, :body))
@@ -17,12 +28,30 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
-  def edit; end
+  def edit
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def update
+    @comment = @post.comments.find(params[:id])
+    @comment.update_attributes(comment_params)
+    if @comment.save
+        redirect_to @post
+    else
+        render 'edit'
+    end
+  end
 
   private
 
   def comment_params
     params.require(:user).permit(:body, :commenter, :post_id)
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:post_id])
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+
+  resources :comments
+  
   # devise_for :users
   # devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in'}
 
