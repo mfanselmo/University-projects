6c0d67739edf7b86df0fefbddfe48d790ecaa88b
RODRIGO HANUCH <rihanuch@uc.cl>
2018-05-10 15:21:24 -0300

    fix error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,7 +10,7 @@ class Comment < ApplicationRecord
     self[attribute] ||= 0
     self[attribute] += by
     save
-end
+  end
 
   def decrement(attribute, by = 1)
     self[attribute] ||= 0
@@ -21,7 +21,10 @@ end
   def self.search(search)
     where("commenter LIKE ? OR body LIKE ?", "%#{search}%", "%#{search}%")
     # where("name LIKE ? OR ingredients LIKE ? OR cooking_instructions LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
+  end
+
   def points
     return self.get_upvotes.size - self.get_downvotes.size
   end
+  
 end
