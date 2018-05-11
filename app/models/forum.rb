ed8d40790d26db40d49fb21976f2156a69447083
Martin Anselmo <mfanselmo@uc.cl>
2018-05-11 15:27:25 -0300

    Fix algunos helper, volvi a agregar una relacion cn through que se habia borrado, add to home forums que moderas

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -3,8 +3,11 @@
 class Forum < ApplicationRecord
   has_many :posts, dependent: :destroy
 
-  has_many :subcriptions
-  has_many :users, through: :subcriptions
+  has_many :subscriptions
+  has_many :users, through: :subscriptions
+
+  has_many :moderators
+  has_many :users, through: :moderators
 
   def self.search(search)
   	where("name LIKE ?", "%#{search}%")
