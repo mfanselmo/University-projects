9429ce5f202b27e9f0b6beb21046b825cba26051
nachocontreras <icontreras1@uc.cl>
2018-05-13 20:58:45 -0400

    delete user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :comments, dependent: :destroy
-  has_many :posts, dependent: :destroy
+  has_many :comments # , dependent: :destroy
+  has_many :posts # , dependent: :destroy
 
   has_many :subscriptions
   has_many :forums, through: :subscriptions
