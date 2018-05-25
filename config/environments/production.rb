8eca7b506878cdfcf0e56daae8c2f7b58f70a85f
nachocontreras <icontreras1@uc.cl>
2018-05-25 14:44:21 -0400

    cable edit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://192.168.99.100:3000/cable'
   # config.action_cable.allowed_request_origins = [ 'http://192.168.99.100:3000/']
-  config.action_cable.url = 'wss://grupo02-software.herokuapp.com/cable'
+  # config.action_cable.url = 'wss://grupo02-software.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ 'https://grupo02-software.herokuapp.com/']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
