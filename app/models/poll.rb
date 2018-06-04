1408fa04c00dbefa950cb6b41f5186e447acc004
nachocontreras <icontreras1@uc.cl>
2018-06-04 15:42:37 -0400

    changes

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,4 +1,5 @@
 class Poll < ApplicationRecord
   has_many :questions, :dependent => :destroy, :inverse_of => :poll
   belongs_to :post, optional: true
+  accepts_nested_attributes_for :questions, dependent: :destroy 
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,4 @@
 class Question < ApplicationRecord
   has_many :attempts
   belongs_to :poll
-  validates_presence_of :content
 end
