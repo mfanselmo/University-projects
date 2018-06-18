b9a431db829b8fb0831d9490271b009a5ef6bdc0
nachocontreras <icontreras1@uc.cl>
2018-06-18 15:38:41 -0400

    add mailer

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,7 +4,7 @@ class Subscription < ApplicationRecord
   belongs_to :user
   belongs_to :forum
   # Descomentar lo siguiente para despues de seeds
-  # after_create :subscription_send  
+  after_create :subscription_send  
   def subscription_send
   	begin
     	EmailerMailer.subscription_mail(user, forum).deliver_now
